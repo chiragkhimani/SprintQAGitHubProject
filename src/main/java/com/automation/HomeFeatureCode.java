588c4cb84298358ea3565e249bb5c8642357d9ce
khimanichirag <khimanichirag@gmail.com>
2021-03-28 01:07:39 +0530

    From Chirag Side Commit

--- src/main/java/com/automation/HomeFeatureCode.java
+++ src/main/java/com/automation/HomeFeatureCode.java
@@ -4,5 +4,6 @@ public class HomeFeatureCode {
 	public static void main(String[] args) {
 		System.out.println("home feature code");
 		System.out.println("Something from chirag");
+		System.out.println("From Chirag Again for 2nd scenario");
 	}
 }
