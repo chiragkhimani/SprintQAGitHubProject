62d0a6abab1c718a50fc3e0c1ee62fe73d4ef41e
khimanichirag <khimanichirag@gmail.com>
2021-03-28 00:31:34 +0530

    from chirag

--- src/main/java/com/automation/HomeFeatureCode.java
+++ src/main/java/com/automation/HomeFeatureCode.java
@@ -3,5 +3,6 @@ package com.automation;
 public class HomeFeatureCode {
 	public static void main(String[] args) {
 		System.out.println("home feature code");
+		System.out.println("Something from chirag");
 	}
 }
